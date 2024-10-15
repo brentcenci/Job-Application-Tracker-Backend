27e565d9d2d860a05c8613f028f2941a27ee0b6e
Brent <bcen0001@student.monash.edu>
2024-10-15 14:02:58 +1100

    Added more job application fields

--- src/main/kotlin/com/example/data/JobApplication.kt
+++ src/main/kotlin/com/example/data/JobApplication.kt
@@ -2,12 +2,18 @@ package com.example.data
 
 import org.bson.codecs.pojo.annotations.BsonId
 import org.bson.types.ObjectId
+import java.time.LocalDateTime
 
 data class JobApplication(
     @BsonId val jobId: ObjectId? = null,
     val userId: ObjectId = ObjectId("64ff9c8e7a3b3e4f9c6a3b32"),
     val jobTitle: String,
+    val jobLevel: String,
     val companyName: String,
+    val industry: String,
     val applicationDate: String,
+    val updateDate: String = LocalDateTime.now().toString(),
     val status: String,
+    val source: String,
+    val url: String = "",
 )
